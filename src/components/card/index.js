eb60143de9d3d788886bad6b72289e0b4d5d10a8
LucasViniciusLima <lucaslimavzt@gmail.com>
2022-06-02 10:00:58 -0300

    first commit

---
+++ src/components/card/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export const Card = (props) => {
+    return (
+        <View>
+            <Text>Card works</Text>
+        </View>
+    );
+}
