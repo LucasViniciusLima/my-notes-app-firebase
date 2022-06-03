65786db82e2e7bb9856e31c820791e3f00e66c6a
LucasBezerra <lucaslimavzt@gmail.com>
2022-06-03 14:41:58 -0300

    img change auto

--- src/components/card-modal-edit/index.js
+++ src/components/card-modal-edit/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native';
 import { doc, setDoc } from 'firebase/firestore'
 
 import { db } from '../../database/firebaseDb';
@@ -7,12 +7,11 @@ import { db } from '../../database/firebaseDb';
 export const CardModalEdit = () => {
     const [noteTitle, setNoteTitle] = useState('');
     const [noteBodyText, setNoteBodyText] = useState('');
-    const [imgUrl, setImgUrl] = useState('');
+    const [imgUrl, setImgUrl] = useState('https://i.pinimg.com/originals/27/1d/18/271d1863de5a7e0a88b5c8023c61dc01.jpg');
 
     const Create = () => {
-        const myDoc = doc(db, "notes", "newDocument2");//auto change doc
+        const myDoc = doc(db, "notes", Math.floor(Date.now() * Math.random()).toString(36) );
         const docData = {
-            "name": "UserName",//change username
             "noteTitle": noteTitle,
             "noteBodyText": noteBodyText,
             "date": new Date(),
@@ -21,43 +20,44 @@ export const CardModalEdit = () => {
         setDoc(myDoc, docData)
             .then(() => {
                 alert("Gravado com sucesso.");//change to close modal instead
-                setName('');
                 setNoteTitle('');
                 setNoteBodyText('');
-                setImgUrl('');
+            })
+            .catch((error) => {
+                alert("erro ao gravar");
             });
     }
 
     return (
         <View style={myStyle.container}>
             <View style={myStyle.bloco1}>
-
+                <ImageBackground source={{uri: imgUrl}} style={myStyle.imgBlock}>
+                    <Text style={[myStyle.textWhite, myStyle.myTitle]}>{noteTitle}</Text>
+                </ImageBackground>
             </View>
             <View style={myStyle.bloco2}>
-            <Text style={myStyle.myTitle} >Criar nova anotação</Text>
-            <TextInput //add a stylesheet for the forms
-                style={myStyle.myInput}
-                onChangeText={iUrl => setImgUrl(iUrl)}//try change background in same time
-                value={imgUrl}
-                placeholder={"Url da foto (background)"}>
-            </TextInput>
-            <TextInput
-                style={myStyle.myInput}
-                onChangeText={noteT => setNoteTitle(noteT)}
-                value={noteTitle}
-                placeholder={"Titulo da anotação"}>
-            </TextInput>
-            <TextInput
-                style={myStyle.myInput}
-                onChangeText={bText => setNoteBodyText(bText)}
-                value={noteBodyText}
-                placeholder={"Corpo da anotação.."}
-                multiline
-                numberOfLines={4}>
-            </TextInput>
-            <Button onPress={Create} title="Salvar Anotação"></Button>
+                <Text style={myStyle.myTitle} >Criar nova anotação</Text>
+                <TextInput
+                    style={myStyle.myInput}
+                    onChangeText={iUrl => setImgUrl(iUrl)}
+                    placeholder={"Url da foto (background)"}>
+                </TextInput>
+                <TextInput
+                    style={myStyle.myInput}
+                    onChangeText={noteT => setNoteTitle(noteT)}
+                    value={noteTitle}
+                    placeholder={"Titulo da anotação"}>
+                </TextInput>
+                <TextInput
+                    style={myStyle.myInput}
+                    onChangeText={bText => setNoteBodyText(bText)}
+                    value={noteBodyText}
+                    placeholder={"Corpo da anotação.."}
+                    multiline
+                    numberOfLines={4}>
+                </TextInput>
+                <Button onPress={Create} title="Salvar Anotação"></Button>
             </View>
-            
         </View>
     );
 }
@@ -73,19 +73,29 @@ const myStyle = StyleSheet.create({
     },
     container:{
         flex: 1,
-        width: "100%"
+        alignSelf: 'stretch',
     },
     bloco1: {
-        flex: 1,
+        flex: 1.5,
         backgroundColor: "#111"
     },
     bloco2: {
         flex: 4,
-        padding: 20
+        paddingHorizontal: 20,
+        paddingVertical: 50
     },
     myTitle: {
         fontSize: 22,
         marginBottom: 15,
-        fontWeight: 750
+        fontWeight: "bold"
+    },
+    textWhite: {
+        color: "#FFFFFF"
+    },
+    imgBlock: {
+        flex: 1,
+        alignSelf: 'stretch',
+        paddingTop: 125,
+        paddingHorizontal: 20
     }
 });
